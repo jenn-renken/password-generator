540a0e50461f3285ca3930379912a4e662764fb1
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-09-09 17:31:15 -0400

    write random password generation function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # password-generator
+
+## Features
+- Select a length between 8 and 128 characters
+    - Selection is validated
+- Select whether to use special characters, uppercase, lowercase and numbers in the password
+    - Validation is included to make sure at least one selection is made
+- Password is randomly generated using the choices selected by the user
+
--- script.js
+++ script.js
@@ -1,9 +1,63 @@
-// Assignment Code
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+// password variable values
+lowerCase = "abcdefghijklmnopqrstuvwxyz";
+upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+number = "0123456789";
+character = "!@#$%^&*-+=";
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // Most of the code will go inside this function!
-  return 'superSecretPassword';
+
+  enter = parseInt(window.prompt("Choose a character length between 8 and 128 characters"));
+    // validation
+    if (!enter) {
+      alert("This needs a value");
+
+    } else if (enter < 8 || enter > 128) {
+      enter = parseInt(prompt("You must choose between 8 and 128 characters"));
+
+    } else {
+      confirmNumber = confirm("Will this contain numbers?");
+      confirmCharacter = confirm("Will this contain special characters?");
+      confirmUppercase = confirm("Will this contain uppercase letters?");
+      confirmLowercase = confirm("Will this contain lowercase letters?");
+    };
+
+    // validation for no selection
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+      alert("You must choose criteria!");
+    }
+
+    var choices = "";
+
+      if (confirmCharacter) {
+        choices += character;
+      }
+      if (confirmNumber) {
+        choices += number;
+      }
+      if (confirmUppercase) {
+        choices += upperCase;
+      }
+      if (confirmLowercase) {
+        choices += lowerCase;
+      }
+
+  var password = "";
+
+  // random selection
+  for (var i =0; i < enter; i++) {
+    var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+    password += pickChoices;
+  }
+
+  return password;
 }
 
 // Write password to the #password input
